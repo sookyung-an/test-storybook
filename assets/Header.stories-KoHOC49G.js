8b497179a57a6ba0dd927453ec1b6c43f6044f4f
sookyung-an <sk.an@catenoid.net>
2025-03-26 02:00:28 +0000

    chore: deploy storybook from main 🚀

---
+++ assets/Header.stories-KoHOC49G.js
@@ -0,0 +1,7 @@
+import{f as o}from"./index-BZkcKs8Z.js";import{H as p}from"./Header-B8DmYrmB.js";import"./jsx-runtime-DoEZbXM1.js";import"./jsx-runtime-Bw5QeaCk.js";import"./Button-C65Sql_F.js";import"./index-D6ByhKmv.js";import"./index-CF1IiRVY.js";const x={title:"Example/Header",component:p,tags:["autodocs"],parameters:{layout:"fullscreen"},args:{onLogin:o(),onLogout:o(),onCreateAccount:o()}},e={args:{user:{name:"Jane Doe"}}},r={};var a,s,t;e.parameters={...e.parameters,docs:{...(a=e.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  args: {
+    user: {
+      name: 'Jane Doe'
+    }
+  }
+}`,...(t=(s=e.parameters)==null?void 0:s.docs)==null?void 0:t.source}}};var n,m,c;r.parameters={...r.parameters,docs:{...(n=r.parameters)==null?void 0:n.docs,source:{originalSource:"{}",...(c=(m=r.parameters)==null?void 0:m.docs)==null?void 0:c.source}}};const H=["LoggedIn","LoggedOut"];export{e as LoggedIn,r as LoggedOut,H as __namedExportsOrder,x as default};
